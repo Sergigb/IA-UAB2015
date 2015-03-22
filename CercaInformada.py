486a545c7213dbad49f9e4ed23dabca6eeb9ad5c
Sergigb <sergi.garciab@e-campus.uab.cat>
2015-03-22 13:01:23 +0100

    La funcó AstarAlgorithm busca la estació d'orígen i destí més propera

--- CercaInformada.py
+++ CercaInformada.py
@@ -15,13 +15,16 @@
 # _________________________________________________________________________________________
 
 from MapaMetro import *
+import math
 
 
 
 
 def RemoveCycles(childrenList):
+    pass
 
 def RemoveRedundantPaths(childrenList, nodeList, partialCostTable):
+    pass
 
 
 def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, typePreference, timeTransfers,
@@ -49,8 +52,101 @@ def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, ty
 
             optimalPath.time, optimalPath.walk, optimalPath.transfers, optimalPath.num_stopStation, len(
         expandedList), len(optimalPath.parentsID), visitedNodes, idsOptimalPath, min_distance_origin, min_distance_destination
+
+
+        class Station:
+    # __init__ Constructor of Station Class.
+    def __init__(self, id, name, line, x, y):
+        self.id = id  #station id
+        self.destinationDic = {}  #Dictionary where principal keys refers to the set of stations that it is connected.
+        #The value of this dictionary refers to the time cost between two stations.
+        self.name = name  #station Name
+        self.line = line  # line name string
+        self.x = x  # coordinate X of the station
+        self.y = y  # coordinate Y of the station
+
+
+
+coord_origin, coord_destination   <---- coordenades
+        
     """
+    #Busquem l'estació més propera al orígen
+    closestStationOrigin = None
+
+    for station in stationList:
+        
+        if(closestStationOrigin != None):
+            distance_ = math.sqrt( (coord_origin[0] - station.x)**2 + (coord_origin[1] - station.y)**2 )
+            print "distance: %d", distance
+            print "distance_: &d", distance_
+            
+            if(distance_ < distance):
+                closestStationOrigin = station
+                distance = distance_
+        else:
+            print "one time"
+            closestStationOrigin = station
+            distance = math.sqrt( (coord_destination[0] - station.x)**2 + (coord_destination[1] - station.y)**2 )
 
 
-if __name__ == "__main__":
-    main()
+
+    closestStationDestination = None
+        
+    for station in stationList:
+        
+        if(closestStationDestination != None):
+            distance_ = math.sqrt( (coord_destination[0] - station.x)**2 + (coord_destination[1] - station.y)**2 )
+            if(distance_ < distance):
+                closestStationDestination = station
+                distance = distance_
+        else:
+            closestStationDestination = station
+            distance = math.sqrt( (coord_destination[0] - station.x)**2 + (coord_destination[1] - station.y)**2 )
+            
+
+
+
+
+        
+    print("L'estacio mes proxima al origen es ", closestStationOrigin.name)
+    print("L'estacio mes proxima al desti es ", closestStationDestination.name)
+    
+
+
+
+
+
+    
+
+
+#if __name__ == "__main__":
+#    main()
+
+
+
+def test(coord_origin, coord_destination, typePreference):    #coord_origin i coord_destination s'han de cridar com una tupla com per exempe: (coord_destination_x, coord_destination_y)
+
+    stationList = readStationInformation("Stations.txt")      #Llista d'estacions
+    matAdjacencia = readCostTable("Connections.txt")          #Matriu d'adjacència de les estacions 
+    timeStations = readCostTable("TempsEstacions.txt")        #Matriu de costos entre estacions
+    timeTransfers = readCostTable("TempsTransbordaments.txt") #Matriu de costos de transbord
+    AstarAlgorithm(stationList, matAdjacencia, coord_origin, coord_destination, typePreference, timeTransfers, timeStations)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
