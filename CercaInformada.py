13737cfad8ed3ce32eb20de1d7622aee2a68d1c1
khartuin <danischez@gmail.com>
2015-03-23 16:38:32 +0100

    AstarSearch v1.0

--- CercaInformada.py
+++ CercaInformada.py
@@ -60,9 +60,13 @@ def TimeHeuristic(source,destination,estimatedSpeed):
     time = DistanceHeuristic(source,destination)/estimatedSpeed
     return time
 
-def StationHeuristic(source,destination,maxDist):
-    numStations = math.hypot((destination.x - source.x),(destination.y - source.y))//maxDist
-    return int(numStations)
+def StationHeuristic(target,destination):
+    if target == destination:
+        return 0
+    elif target.line == destination.line:
+        return 1
+    else:
+        return 2
 
 def TransferHeuristic(source,destination): #Si no estem a la mateixa línea, haurem de fer com a mínim un transbord
     if source.line != destination.line:
@@ -144,7 +148,6 @@ def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, ty
 
     List = [[(closestStationOrigin.id, 0)]]      #El primer element de la llista de camins és el primer node
     currentNode = (None, None)
-    maxDist = 0
 
     while List and currentNode[0] != closestStationDestination.id:          #Mentres la llista no estigui buida o el primer node del primer camí(path) no sigui el destí, seguim expandint
         
@@ -159,24 +162,20 @@ def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, ty
 
             if typePreference == 0:
                 partialCost = math.hypot((nextStation.x - currentStation.x),(nextStation.y - currentStation.y))
-                h = DistanceHeuristic(currentStation,closestStationDestination)
+                h = DistanceHeuristic(nextStation,closestStationDestination)
                 partialCost = partialCost + h
                 
             elif typePreference == 1:
-                dist = math.hypot((nextStation.x - currentStation.x),(nextStation.y - currentStation.y))
-                if  dist > maxDist:
-                    maxDist = dist
-                h = StationHeuristic(currentStation,closestStationDestination,maxDist)
-                partialCost = 1 + h
+                partialCost = StationHeuristic(nextStation,closestStationDestination)
                 
             elif typePreference == 2:
                 partialCost = timeStations[currentStation.id][nextStation.id]
-                estimatedSpeed = (math.hypot((nextStation.x - currentStation.x),(nextStation.y - currentStation.y)))/partialCost #V=x/t assumim que la velocitat serà constant
+                estimatedSpeed = (math.hypot((nextStation.x - currentStation.x),(nextStation.y - currentStation.y)))/partialCost #V=x/t: assumim que la velocitat serà constant
 
                 if currentStation.line != nextStation.line:
                     partialCost = partialCost + timeTransfers[currentStation.id][nextStation.id]
                     
-                h = TimeHeuristic(currentStation,nextStation,estimatedSpeed)
+                h = TimeHeuristic(nextStation,closestStationDestination,estimatedSpeed)
                 partialCost = partialCost + h
                 
             elif typePreference == 3:
