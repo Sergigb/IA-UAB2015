4202ac4d8cac9634fb4c608c79c3c7a13acdb432
khartuin <danischez@gmail.com>
2015-03-23 16:45:45 +0100

    Modificaciones en implementaciones de partialCost

--- CercaInformada.py
+++ CercaInformada.py
@@ -166,7 +166,9 @@ def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, ty
                 partialCost = partialCost + h
                 
             elif typePreference == 1:
-                partialCost = StationHeuristic(nextStation,closestStationDestination)
+                partialCost = 1
+                h = StationHeuristic(nextStation,closestStationDestination)
+                partialCost = partialCost + h
                 
             elif typePreference == 2:
                 partialCost = timeStations[currentStation.id][nextStation.id]
@@ -179,7 +181,12 @@ def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, ty
                 partialCost = partialCost + h
                 
             elif typePreference == 3:
-                partialCost = TransferHeuristic(currentStation,nextStation)
+                if currentStation.line != nextStation.line:
+                    partialCost = 1
+                else:
+                    partialCost = 0
+                h = TransferHeuristic(nextStation,closestStationDestination)
+                partialCost = partialCost + h
                 
             else:
                 raise AttributeError
@@ -206,28 +213,28 @@ def AstarAlgorithm(stationList, connections, coord_origin, coord_destination, ty
         #print List
         if currentNode[0] == closestStationDestination.id:                  #Si el primer node del primer camí és el destí, l'imprimim (i després la funció acabará)
 
-            if len(path) > 2: 
-                i = 0
-                for item in path:                                               #Elimina les estacions del principi de la ruta que estiguin repetides
-                    nextItem = path[i+1]
-                    station = stationList[item[0]-1]
-                    station_ = stationList[nextItem[0]-1]
-                    if station.name == station_.name:
-                        path.pop(i)
-                    else:
-                        break
-                    i+=1
-    
-                i = len(path) - 1                                               #Elimina les estacions del final de la ruta que estiguin repetides
-                for item in reversed(path):
-                    nextItem = path[i-1]
-                    station = stationList[item[0]-1]
-                    station_ = stationList[nextItem[0]-1]
-                    if station.name == station_.name:
-                        path.pop(i)
-                    else:
-                        break
-                    i-=1
+            i = 0
+
+            for item in path:                                               #Elimina les estacions del principi de la ruta que estiguin repetides
+                nextItem = path[i+1]
+                station = stationList[item[0]-1]
+                station_ = stationList[nextItem[0]-1]
+                if station.name == station_.name:
+                    path.pop(i)
+                else:
+                    break
+                i+=1
+
+            i = len(path) - 1                                               #Elimina les estacions del final de la ruta que estiguin repetides
+            for item in reversed(path):
+                nextItem = path[i-1]
+                station = stationList[item[0]-1]
+                station_ = stationList[nextItem[0]-1]
+                if station.name == station_.name:
+                    path.pop(i)
+                else:
+                    break
+                i-=1
 
             print "HEAD PATH", path
             for node in path:
